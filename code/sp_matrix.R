1e349261ee2d96f7e647dd2f449b0fd6a1330552
Marie Gutgesell <marie.gutgesell11@gmail.com>
2024-09-17 08:20:42 -0800

    create array without mixed site

--- code/sp_matrix.R
+++ code/sp_matrix.R
@@ -10,7 +10,13 @@ df <- read.csv("data/SEAK_Inverts_IndMeasure_AbunTakeMeanBL.csv")
 df_summary <- df %>%
   select(StreamID, Date, Origin.x, Order.x, Family.x, Genus, Biomass_Ind) %>%
   group_by(StreamID, Date, Origin.x, Order.x, Family.x) %>% ##family is lowest level ID for all taxa
-  summarise_at(vars(Biomass_Ind), list(total_biomass = sum))
+  summarise_at(vars(Biomass_Ind), list(total_biomass = sum)) %>%
+  mutate(site_type = case_when(
+    startsWith(StreamID, "Herbert") ~ "Glacial",
+    startsWith(StreamID, "Steep") ~ "Snow",
+    startsWith(StreamID, "Peter") ~ "Rain", 
+    startsWith(StreamID, "Montana") ~ "Mixed",
+  ))
 
 df_summary$Date <- as.Date(df_summary$Date, "%m/%d/%y")
 
@@ -21,13 +27,24 @@ ggplot(df_summary, aes(x = Date, y = total_biomass, color = Family.x)) +
   theme_classic() +
   facet_wrap(~StreamID)
 
+##Remove mixed site 
+df_summary_2 <- df_summary %>%
+  filter(site_type != "Mixed")
 
 # Ensure dataframe is in the correct format
 df_summary <- df_summary %>%
   arrange(Family.x, Date, StreamID)
 
 # Create the array
-sp_matrix <- xtabs(total_biomass ~ Family.x + Date + StreamID, data = df_summary)
+sp_matrix_all <- xtabs(total_biomass ~ Family.x + Date + StreamID, data = df_summary)
+
+
+df_summary_2 <- df_summary_2 %>%
+  arrange(Family.x, Date, StreamID)
+sp_matrix_2 <- xtabs(total_biomass ~ Family.x + Date + StreamID, data = df_summary_2)
+
+
+
 
 
 ##I think blanks are already 0..indicating no abundance at particular time
